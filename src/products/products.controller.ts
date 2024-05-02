40713b9cd7a2d719edc510c9b666d2eebf01be54
Jonda-HR <jonathan@contadito.com>
2024-05-02 01:14:51 -0600

    consumir microservicio

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -5,11 +5,17 @@ import {
   Get,
   Inject,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common';
 import { PRODUCT_SERVICE } from 'src/config';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -18,28 +24,51 @@ export class ProductsController {
   ) {}
 
   @Post()
-  public async create() {
-    return 'Crea un producto';
+  public async create(@Body() createProductDto: CreateProductDto) {
+    return this.productClient.send({ cmd: 'create_product' }, createProductDto);
   }
 
   @Get()
-  public async findAll() {
-    return this.productClient.send({ cmd: 'find_all_products' }, {});
+  public async findAll(@Query() paginationDto: PaginationDto) {
+    return this.productClient.send({ cmd: 'find_all_products' }, paginationDto);
   }
 
   @Get(':id')
-  public async findOne(@Param('id') id: string) {
-    return `Regresa el product con id ${id} `;
+  public async findOne(@Param('id', ParseIntPipe) id: number) {
+    try {
+      const product = await firstValueFrom(
+        this.productClient.send({ cmd: 'find_one_product' }, { id }),
+      );
+
+      return product;
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
   @Delete(':id')
-  public async delete(@Param('id') id: string) {
-    return `Elimina el product con id ${id} `;
+  public async delete(@Param('id', ParseIntPipe) id: number) {
+    return this.productClient.send({ cmd: 'soft_delete_product' }, { id }).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
   }
 
   @Patch(':id')
-  public async update(@Param('id') id: string, @Body() body: any) {
-    console.log(body);
-    return `Actualiza el product con id ${id} `;
+  public async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    try {
+      return await firstValueFrom(
+        this.productClient.send(
+          { cmd: 'update_product' },
+          { id, ...updateProductDto },
+        ),
+      );
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 }
